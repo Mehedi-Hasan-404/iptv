cfe22fcdd992fd6fd94903af260384167836af6e
Mehedi-Hasan-404 <880188mm@gmail.com>
2025-09-21 22:04:45 +0600

    Update CategoryCard.tsx

--- src/components/main/CategoryCard.tsx
+++ src/components/main/CategoryCard.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 import { Category } from '@/types';
 
 const CategoryCard = ({ category }: { category: Category }) => (
-  <Link href={`/category/${category.id}`} className="category-card">
+  <Link href={`/category/${category.slug}`} className="category-card">
     <Image src={category.iconUrl} alt={category.name} width={64} height={64} />
     <span>{category.name}</span>
   </Link>
