f758dc9ebbec6e2a9166738d4c6c366c2966ac05
Mehedi-Hasan-404 <880188mm@gmail.com>
2025-09-25 15:36:57 +0600

    Update Header.tsx

--- src/components/main/Header.tsx
+++ src/components/main/Header.tsx
@@ -1,12 +1,13 @@
+// /src/components/main/Header.tsx
 import { LogoIcon } from './Icons';
 
 const Header = ({ onMenuClick }: { onMenuClick: () => void }) => (
   <header>
-    <span className="menu-btn" onClick={onMenuClick}>☰</span>
     <div style={{ display: 'flex', alignItems: 'center' }}>
       <span className="logo-icon"><LogoIcon /></span>
       <span className="title">Live TV Pro</span>
     </div>
+    <span className="menu-btn" onClick={onMenuClick}>☰</span>
   </header>
 );
 export default Header;
